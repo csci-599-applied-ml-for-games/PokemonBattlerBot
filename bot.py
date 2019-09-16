ab799ed596cf68b380f4dd136e15d5ea068a129b
chapel582 <trappleco@gmail.com>
2019-09-15 18:42:44 -0700

    can now accept challenges

--- bot.py
+++ bot.py
@@ -1,14 +1,55 @@
+import sys
+import os
+
 import showdown 
 
-print(showdown.__file__)
-
-username1 = 'USCBot1'
-password1 = 'USCBot1'
+TEAMS_DIR = os.path.dirname(__file__)
 
 class BotClient(showdown.Client):
+	def __init__(self, name='', password='', loop=None, max_room_logs=5000,
+		server_id='showdown', server_host=None, expected_opponent=None,
+		team=None):
+
+		if expected_opponent == None:
+			raise Exception("No expected opponent found in arguments")
+		else:
+			self.expected_opponent = expected_opponent
+		if team == None:
+			raise Exception("No team found in arguments")
+		else:
+			self.team = team
+
+		super().__init__(name=name, password=password, loop=loop, 
+			max_room_logs=max_room_logs, server_id=server_id, 
+			server_host=server_host)
+
 	async def on_private_message(self, pm):
 		if pm.recipient == self:
-			print(pm.content)
 			await pm.reply(pm.content)
 
-BotClient(name=username1, password=password1).start()
+	async def on_challenge_update(self, challenge_data):
+		incoming = challenge_data.get('challengesFrom', {})
+		if self.expected_opponent.lower() in incoming:
+			print("expected_opponent found")
+			await self.accept_challenge(self.expected_opponent, self.team)
+
+	async def on_room_init(self, room_obj):
+		print(room_obj.id)
+
+def main():
+	if len(sys.argv) != 4:
+		print('Usage: python bot.py <username> <password> <expected_opponent>')
+		return 
+
+	username = sys.argv[1]
+	password = sys.argv[2]
+	expected_opponent = sys.argv[3]
+
+	with open(os.path.join(TEAMS_DIR, 'teams/PokemonTeam'), 'rt') as teamfd:
+		team = teamfd.read()
+
+	BotClient(name=username, password=password, 
+		expected_opponent=expected_opponent, team=team).start()
+
+if __name__ == '__main__':
+	main()
