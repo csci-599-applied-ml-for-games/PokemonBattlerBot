c9ee58498446ae5ad41f2edeaa0efc0c2d4fea49
chapel582 <trappleco@gmail.com>
2019-09-15 19:47:33 -0700

    able to start with pokemon 1 by default

--- bot.py
+++ bot.py
@@ -23,6 +23,28 @@ class BotClient(showdown.Client):
 			max_room_logs=max_room_logs, server_id=server_id, 
 			server_host=server_host)
 
+	async def on_receive(self, room_id, inp_type, params):
+		"""
+		|coro|
+
+		Hook for subclasses. Called when the client receives any data from the
+		server.
+	
+		Args:
+			room_id (:obj:`str`) : ID of the room with which the information is 
+				associated with. Messages with unspecified IDs default to '
+				lobby', though may not necessarily be associated with 'lobby'.
+			inp_type (:obj:`str`) : The type of information received.
+				Ex: 'l' (user leave), 'j' (user join), 'c:' (chat message)
+			params (:obj:`list`) : List of the parameters associated with the 
+				inp_type. Ex: a user leave has params of ['zarel'], where 'zarel'
+				represents the user id of the user that left.
+
+		Notes:
+			Does nothing by default.
+		"""
+		pass
+
 	async def on_private_message(self, pm):
 		if pm.recipient == self:
 			await pm.reply(pm.content)
@@ -30,11 +52,11 @@ class BotClient(showdown.Client):
 	async def on_challenge_update(self, challenge_data):
 		incoming = challenge_data.get('challengesFrom', {})
 		if self.expected_opponent.lower() in incoming:
-			print("expected_opponent found")
 			await self.accept_challenge(self.expected_opponent, self.team)
 
 	async def on_room_init(self, room_obj):
-		print(room_obj.id)
+		if room_obj.id.startswith('battle-'):
+			await room_obj.start_poke(1)
 
 def main():
 	if len(sys.argv) != 4:
