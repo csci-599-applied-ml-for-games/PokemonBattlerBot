c567579d3727e1334c843ee044b6a1e01df4c16c
chapel582 <trappleco@gmail.com>
2019-09-16 21:42:17 -0700

    we no longer assume that we have four moves

--- bot.py
+++ bot.py
@@ -29,56 +29,62 @@ class BotClient(showdown.Client):
 		print(inp_type)
 		print(params)
 
-		room_obj = self.rooms[room_id]				
-		if inp_type == 'poke':
-			owner = params[0]
-			pokename = params[1]
-			if owner == self.position:
-				self.team.append(pokename)
-			else:
-				self.opp_team.append(pokename)
+		room_obj = self.rooms[room_id]	
+		if room_obj.id.startswith('battle-'):
+			if inp_type == 'poke':
+				owner = params[0]
+				pokename = params[1]
+				if owner == self.position:
+					self.team.append(pokename)
+				else:
+					self.opp_team.append(pokename)
 
-			if (len(self.team) == self.teamsize and
-				len(self.opp_team) == self.opp_teamsize):
+				if (len(self.team) == self.teamsize and
+					len(self.opp_team) == self.opp_teamsize):
 
-				print(f'Team: {self.team}')
-				print(f'Opp team: {self.opp_team}')
-				
-				#NOTE: Select starting pokemon here 
-				start_index = random.randint(1, self.teamsize)
-				await room_obj.start_poke(start_index)
+					print(f'Team: {self.team}')
+					print(f'Opp team: {self.opp_team}')
+					
+					#NOTE: Select starting pokemon here 
+					start_index = random.randint(1, self.teamsize)
+					await room_obj.start_poke(start_index)
 
-		elif inp_type == 'teamsize':
-			position = params[0]
-			if position == self.position:
-				self.teamsize = int(params[1])
-				self.team = []
-			else:
-				self.opp_teamsize = int(params[1])
-				self.opp_team = []
+			elif inp_type == 'teamsize':
+				position = params[0]
+				if position == self.position:
+					self.teamsize = int(params[1])
+					self.team = []
+				else:
+					self.opp_teamsize = int(params[1])
+					self.opp_team = []
 
-		elif inp_type == 'player':
-			name = params[1]
-			position = params[0] 
-			if name == self.name:
-				self.position = position
+			elif inp_type == 'player':
+				name = params[1]
+				position = params[0] 
+				if name == self.name:
+					self.position = position
 
-		elif inp_type == 'turn':
-			await room_obj.move(random.randint(1, 4)) #TODO: Don't assume we have four moves 
-			
-		elif inp_type == 'request':
-			json_string = params[0]
-			data = json.loads(json_string)
-			force_switch = data.get('forceSwitch', [False])[0]
-			if force_switch == True: #TODO: can this request arrive when opponent's pokemon faints?
-				print("FORCE SWITCH")
-				switch_available = []
-				team_info = data['side']['pokemon']
-				for pokemon_index, pokemon_info in enumerate(team_info):
-					if 'fnt' not in pokemon_info['condition']:
-						switch_available.append(pokemon_index + 1)
-				switch_index = random.choice(switch_available)
-				await room_obj.switch(switch_index)
+			elif inp_type == 'turn':
+				self.turn_number = int(params[0])
+
+			elif inp_type == 'request':
+				json_string = params[0]
+				data = json.loads(json_string)
+				force_switch = data.get('forceSwitch', [False])[0]
+				if force_switch == True: #TODO: can this request arrive when opponent's pokemon faints?
+					print("FORCE SWITCH")
+					switch_available = []
+					team_info = data['side']['pokemon']
+					for pokemon_index, pokemon_info in enumerate(team_info):
+						if 'fnt' not in pokemon_info['condition']:
+							switch_available.append(pokemon_index + 1)
+					switch_index = random.choice(switch_available)
+					await room_obj.switch(switch_index)
+				else:
+					moves = data.get('active')[0].get('moves')
+					move_count = len(moves)
+					# action_count = move_count
+					await room_obj.move(random.randint(1, move_count)) 
 
 	async def on_private_message(self, pm):
 		if pm.recipient == self:
